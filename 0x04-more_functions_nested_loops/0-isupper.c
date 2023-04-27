2c9a343290bab74a7fa0c1f588da0b78e0eaa909
AmiraMesalami <amiraa.elmosallamy@gmail.com>
2023-04-27 12:55:03 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,21 +12,3 @@ int _isupper(int c)
 	else
 		return (0);
 }
-
-
-1-isdigit.c
-
-#include "main.h"
-
-/**
- * _isdigit -  a function that checks for a digit 0-9
- * @c: input
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(int c)
-{
-	if ((c >= 48) && (c <= 57))
-		return (1);
-	else
-		return (0);
-}
