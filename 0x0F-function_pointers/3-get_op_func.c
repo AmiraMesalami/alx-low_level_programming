b8ca9f9eeefd525efb3baf0e36a7650d70aade5f
AmiraMesalami <amiraa.elmosallamy@gmail.com>
2023-05-24 04:11:15 -0700

    update me

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,73 +1,33 @@
-#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * op_add - addition
- * @a: parameter passed from main
- * @b: parameter passed from main
+ * get_op_func - function pointer to the correct function to perform the
+ * operation asked by the user
+ * @s: operator passed as argument to the program
  *
- * Return: result
+ * Return: a pointer to the function that corresponds to the operator
+ * given as a parameter
  */
 
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-/**
- * op_sub - substraction
- * @a: parameter passed from main
- * @b: parameter passed from main
- *
- * Return: result
- */
-
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * op_mul - multiplication
- * @a: parameter passed from main
- * @b: parameter passed from main
- *
- * Return: result
- */
-
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - division
- * @a: parameter passed from main
- * @b: parameter passed from main
- *
- * Return: result
- */
-
-int op_div(int a, int b)
-{
-	if (b != 0)
-		return (a / b);
-	printf("Error\n"), exit(100);
-}
-
-/**
- * op_mod - modulus
- * @a: parameter passed from main
- * @b: parameter passed from main
- *
- * Return: result
- */
-
-int op_mod(int a, int b)
-{
-	if (b != 0)
-		return (a % b);
-	printf("Error\n"), exit(100);
+	while (i < 5)
+	{
+		if (*s == *ops[i].op)
+			return (*(ops[i]).f);
+		i++;
+	}
+	return (NULL);
 }
